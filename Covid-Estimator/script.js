97a12d4c3da2c0aa4446d318236acfebe9785372
Faith Idowu <faithid10@gmail.com>
2020-04-25 18:22:46 +0100

    chore(Day 13): Entry for frontendmentor.io challenge Day 13 - Huddle, full page

--- Covid-Estimator/script.js
+++ Covid-Estimator/script.js
@@ -1,4 +1,18 @@
 import estimator from "./estimator.js";
+const sample = {
+	region: {
+		name: "Africa",
+		avgAge: 19.7,
+		avgDailyIncomeInUSD: 5,
+		avgDailyIncomePopulation: 0.71,
+	},
+	periodType: "days",
+	timeToElapse: 58,
+	reportedCases: 674,
+	population: 66622705,
+	totalHospitalBeds: 1380614,
+};
+console.log(estimator(sample));
 
 const form = document.querySelector("form");
 const submitButton = form.querySelector("[data-goestimate]");
