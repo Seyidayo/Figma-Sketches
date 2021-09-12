47486c8d60e79e3a141ddd375910ad0910774c31
seyidayo <faithid10@gmail.com>
2021-09-12 23:13:23 +0100

    :recycle: refactor: code implementation

--- Day 16/script.js
+++
@@ -1,8 +0,0 @@
-function handleMenuToggle() {
-    icon.classList.toggle("show");
-    nav.classList.toggle("show");
-}
-var button = document.querySelector(".menu");
-var nav = document.querySelector(".nav__list");
-var icon = button.querySelector(".icon");
-button.addEventListener("click", handleMenuToggle);
